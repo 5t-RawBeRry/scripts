3c0b594b4e868a29d815181c156644889fc23d0a
Saiki <i@i.ls>
2023-08-27 23:06:28 +0800

    init

--- script.sh
+++ script.sh
@@ -28,27 +28,37 @@ check_root_privileges() {
 
   if ! command -v sudo >/dev/null 2>&1; then
     display_info "Installing sudo..."
-    if [[ -f "/etc/debian_version" ]]; then
-      apt update > /dev/null 2>&1
-      apt install -y sudo > /dev/null 2>&1
-    elif [[ -f "/etc/redhat-release" ]]; then
-      yum install -y sudo > /dev/null 2>&1
-    elif [[ -f "/etc/arch-release" ]]; then
-      pacman -Syu --noconfirm sudo > /dev/null 2>&1
-    else
-      display_error "Unsupported distribution. Please install sudo manually."
-      exit 1
-    fi
-
+    install_package sudo
     if ! command -v sudo >/dev/null 2>&1; then
       display_error "Failed to install sudo. Please install sudo manually."
       exit 1
     fi
-
     display_success "sudo installed successfully."
   fi
 }
 
+# Function to install a package using the appropriate package manager
+install_package() {
+  local package_manager=""
+  if command -v apt >/dev/null 2>&1; then
+    package_manager="apt"
+  elif command -v pacman >/dev/null 2>&1; then
+    package_manager="pacman"
+  elif command -v yum >/dev/null 2>&1; then
+    package_manager="yum"
+  else
+    display_error "Unsupported package manager. Please install the required packages manually."
+    exit 1
+  fi
+
+  display_info "Updating $package_manager package lists..."
+  sudo "$package_manager" update > /dev/null 2>&1
+  display_success "$package_manager package lists updated."
+
+  display_info "Installing required packages..."
+  sudo "$package_manager" install -y "$@" > /dev/null 2>&1
+  display_success "Packages installed."
+}
 
 # Function to create a new user
 create_user() {
@@ -81,44 +91,16 @@ create_user() {
   fi
 }
 
-configure_ssh_key() {
-  local ssh_dir=~/.ssh
-  local authorized_keys="$ssh_dir/authorized_keys"
-  local public_key='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMj1ZURxNE8MV9OkwEYruwBNQDgn61k0u2wQNWIxu7P i@i.ls'
-
-  if [[ -d "$ssh_dir" ]]; then
-    display_success "The \e[1m$ssh_dir\e[0m directory already exists."
-  else
-    display_info "Creating the \e[1m$ssh_dir\e[0m directory..."
-    mkdir -p "$ssh_dir"
-  fi
-
-  if [ -f "$authorized_keys" ]; then
-    display_success "The \e[1m$authorized_keys\e[0m file already exists."
-  else
-    display_info "Creating the \e[1m$authorized_keys\e[0m file..."
-    touch "$authorized_keys"
-  fi
-
-  if grep -q "$public_key" "$authorized_keys"; then
-    display_success "The public key already exists in \e[1mauthorized_keys\e[0m."
-  else
-    echo "$public_key" >> "$authorized_keys"
-    display_success "SSH public key added to \e[1mauthorized_keys\e[0m."
-  fi
-}
-
-
 # Function to configure SSH server
 configure_ssh() {
-  display_info "Updating SSH server configuration..."
+  display_info "Updating SSH server configuration: disabling root login, enabling public key authentication, and disabling password authentication."
   sudo sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' \
          -e 's/#PubkeyAuthentication/PubkeyAuthentication/g' \
          -e 's/#AuthorizedKeysFile/AuthorizedKeysFile/g' \
          -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' \
          /etc/ssh/sshd_config
   display_success "SSH server configuration updated."
-  
+
   display_info "Restarting SSH service..."
   sudo systemctl restart ssh
   display_success "SSH service restarted."
@@ -126,7 +108,7 @@ configure_ssh() {
 
 # Function to configure system settings
 configure_system() {
-  display_info "Updating system configuration..."
+  display_info "Updating system configuration: setting hostname to 'EuangeLion', updating /etc/hosts file, generating locale settings, and setting timezone to Asia/Hong_Kong."
   sudo hostnamectl set-hostname EuangeLion
   sudo sed -i 's/localhost/EuangeLion localhost/g' /etc/hosts
   sudo sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
@@ -135,76 +117,43 @@ configure_system() {
   display_success "System configuration updated."
 }
 
-install_packages_apt() {
-  sudo apt update > /dev/null 2>&1
-  display_success "APT package lists updated."
-
-  display_info "Installing required packages..."
-  sudo apt install -y zsh python3-pip git curl > /dev/null 2>&1
-  display_success "Packages installed."
-}
-
-# Function to install packages using Pacman
-install_packages_pacman() {
-  sudo pacman -Syu --noconfirm zsh python-pip git curl > /dev/null 2>&1
-  display_success "Packages installed."
-}
-
-# Function to install packages using Yum
-install_packages_yum() {
-  sudo yum update -y > /dev/null 2>&1
-  display_success "Yum package lists updated."
-
-  display_info "Installing required packages..."
-  sudo yum install -y zsh python3-pip git curl > /dev/null 2>&1
-  display_success "Packages installed."
-}
-
 # Function to setup the environment
 setup_environment() {
   display_info "Setting up the environment..."
-  
-  if [[ -f "/etc/debian_version" ]]; then
-    install_packages_apt
-  elif [[ -f "/etc/arch-release" ]]; then
-    install_packages_pacman
-  elif [[ -f "/etc/redhat-release" ]]; then
-    install_packages_yum
-  else
-    display_error "Unsupported distribution. Unable to set up the environment."
-    exit 1
-  fi
-  
+
+  install_package zsh curl
+
   display_info "Installing pip..."
-  sudo pip install pip==22.3.1 --break-system-packages > /dev/null 2>&1
+  sudo curl -sSL https://bootstrap.pypa.io/get-pip.py | sudo python3 > /dev/null 2>&1
   display_success "Pip installed."
-  
+
   display_info "Installing wakatime..."
-  pip3 install wakatime > /dev/null 2>&1
+  sudo pip3 install wakatime > /dev/null 2>&1
   display_success "Wakatime installed."
-  
+
   display_info "Configuring Zsh..."
   curl -o ~/.zshrc https://s.repo.host/addons/zshrc > /dev/null 2>&1
   display_success "Zsh configured."
-  
+
   display_info "Changing default shell..."
   sudo chsh -s $(command -v zsh) "$current_user"
   display_success "Default shell changed."
-  
+
   display_success "Environment setup completed successfully."
 
   zsh
 }
 
+# Function to install BBR
 install_bbr() {
   local setup=$1
 
   if [[ -f "/etc/debian_version" ]]; then
     if [[ "$setup" == "-s" ]]; then
       display_success "Enabling BBR..."
-      echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
-      echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
-      sysctl -p
+      echo "net.core.default_qdisc = fq" | sudo tee -a /etc/sysctl.conf > /dev/null
+      echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.conf > /dev/null
+      sudo sysctl -p > /dev/null
       display_success "BBR enabled."
     else
       display_info "Downloading BBR kernel..."
@@ -219,13 +168,14 @@ install_bbr() {
       rm /tmp/linux-*-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb
     fi
   else
-    display_error "Unsupported distribution. Unable to set up the environment."
+    display_error "Unsupported distribution. Unable to install BBR."
     return 1
   fi
 }
 
+# Function to install Docker
 install_docker() {
-  display_info "Installing Docker..."
+  display_info "Installing Docker: checking if Docker is already installed, installing Docker if necessary, and granting Docker privileges to user '$current_user'."
   if command -v docker >/dev/null 2>&1; then
     display_info "Docker is already installed, skipping installation."
   else
@@ -240,19 +190,43 @@ install_docker() {
   fi
 }
 
+# Function to reinstall Debian
 reinstall_debian() {
-  display_info "Installing Debian..."
+  display_info "Installing Debian: using the specified mirror for installation."
+  local mirror="http://deb.debian.org/debian"
   if [[ $(curl -s api.baka.cafe?isCN) == '1' ]]; then
     display_success "Region: \e[1mChina\e[0m, setting the system repo to \e[1mUSTC\e[0m."
-    curl -sSL https://s.repo.host/addons/InstallNET.sh | sudo bash -s -- -d 12 -v 64 -a --mirror 'http://mirrors.ustc.edu.cn/debian' -p 'repo.host'
-  else
-    curl -sSL https://s.repo.host/addons/InstallNET.sh | sudo bash -s -- -d 12 -v 64 -a -p 'repo.host'
-    #display_error "No CN"
+    mirror="http://mirrors.ustc.edu.cn/debian"
   fi
+  curl -sSL https://s.repo.host/addons/InstallNET.sh | sudo bash -s -- -d 12 -v 64 -a --mirror "$mirror" -p 'repo.host'
   display_success "Rebooting..."
 }
 
+# Function to install Caddy
+install_caddy() {
+  display_info "Installing Caddy..."
 
+  display_info "Updating package lists..."
+  sudo apt update > /dev/null 2>&1
+  display_success "Package lists updated."
+
+  install_package gnupg apt-transport-https lsb-release
+
+  curl -sSL https://dl.cloudsmith.io/public/caddy/stable/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/caddy.gpg >/dev/null
+
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/caddy.gpg] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/caddy.list
+
+  display_info "Updating package lists..."
+  sudo apt update > /dev/null 2>&1
+  display_success "Package lists updated."
+
+  install_package caddy
+
+  display_success "Caddy installed successfully."
+
+  display_info "Please configure Caddy by editing the Caddyfile located at /etc/caddy/Caddyfile."
+  display_info "For more information on configuring Caddy, refer to the official Caddy documentation."
+}
 
 declare -g current_user=$(whoami)
 
@@ -281,6 +255,9 @@ case "$1" in
   "bbr")
     install_bbr "$2"
     ;;
+  "caddy")
+    install_caddy
+    ;;
   "create-user")
     if [[ -n "$2" ]]; then
       create_user "$2" "$3"
@@ -290,8 +267,7 @@ case "$1" in
     fi
     ;;
   *)
-    display_error "Invalid argument. Please specify one of the following: \e[1mssh-key, ssh, bbr, docker, system, environment, reinstall, create-user\e[0m"
+    display_error "Invalid argument. Please specify one of the following: \e[1mssh-key, ssh, bbr, caddy, docker, system, environment, reinstall, create-user\e[0m"
     exit 1
     ;;
 esac
-
