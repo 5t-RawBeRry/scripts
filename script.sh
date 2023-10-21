42a015bab16a0f5554544fe10adc52d74c9c21dc
Saiki <i@i.ls>
2023-10-22 02:18:10 +0800

    Refactor the system configuration script for clarity and conciseness
    - Define functions for distinct operations to enhance code readability and reusability.
    
    - Remove comments for a more streamlined script.
    
    - Implement concise condition checks for various configurations.

--- script.sh
+++ script.sh
@@ -1,246 +1,133 @@
 #!/bin/bash
 
-# Colorful print functions
 display_info()    { echo -e "\e[34m[I]\e[0m $1"; }
 display_success() { echo -e "\e[32m[S]\e[0m $1"; }
 display_warning() { echo -e "\e[33m[W]\e[0m $1"; }
 display_error()   { echo -e "\e[31m[E]\e[0m $1"; exit 1; }
 
 print_help() {
-  echo "Usage: $0 <command> [options]"
-  echo ""
-  echo "Available commands:"
-  echo "  ssh-key        Configure the SSH key."
-  echo "  ssh            Configure the SSH server."
-  echo "  docker         Install Docker."
-  echo "  system         Configure system settings."
-  echo "  environment    Setup the environment."
-  echo "  reinstall      Reinstall Debian."
-  echo "  bbr            Install BBR."
-  echo "  caddy          Install Caddy."
-  echo "  create-user    Create a new user."
-  echo ""
-  echo "For more details, specify a command. Example: $0 ssh-key"
+  cat << EOF
+Usage: $0 <command> [options]
+
+Available commands:
+  ssh-key        Configure the SSH key.
+  ssh            Configure the SSH server.
+  docker         Install Docker.
+  system         Configure system settings.
+  environment    Setup the environment.
+  reinstall      Reinstall Debian.
+  bbr            Install BBR.
+  caddy          Install Caddy.
+  create-user    Create a new user.
+
+For more details, specify a command. Example: $0 ssh-key
+EOF
   exit 0
 }
 
-# Check if the operating system is Debian or if the user wants to reinstall Debian
 if [[ ! -f "/etc/debian_version" && "$1" != "reinstall" ]]; then
-  display_error "Unsupported system. This script is designed for Debian systems only."
+  display_error "Unsupported system. Script for Debian only."
 fi
 
 check_root_privileges() {
   if [[ $EUID -ne 0 ]]; then
     sudo -v || display_error "Failed to obtain sudo privileges."
   fi
-  command -v sudo >/dev/null 2>&1 || (install_package sudo && display_success "sudo installed successfully.")
+  command -v sudo >/dev/null 2>&1 || (install_package sudo && display_success "sudo installed.")
 }
 
 install_package() {
-  local package_manager="apt"
-  sudo "$package_manager" update -q > /dev/null
+  sudo apt update -q > /dev/null
   for package in "$@"; do
-    dpkg -l | grep -qw "$package" || (sudo "$package_manager" install -yq "$package" > /dev/null && display_success "Package '$package' installed.")
+    dpkg -l | grep -qw "$package" || (sudo apt install -yq "$package" > /dev/null && display_success "Package '$package' installed.")
   done
 }
 
 configure_ssh_key() {
-  local ssh_dir=~/.ssh
-  local authorized_keys="$ssh_dir/authorized_keys"
-  local public_key='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMj1ZURxNE8MV9OkwEYruwBNQDgn61k0u2wQNWIxu7P i@i.ls'
+  ssh_dir=~/.ssh
+  authorized_keys="$ssh_dir/authorized_keys"
+  public_key='ssh-ed25519 ... i@i.ls'
 
-  if [[ -d "$ssh_dir" ]]; then
-    display_success "The \e[1m$ssh_dir\e[0m directory already exists."
-  else
-    display_info "Creating the \e[1m$ssh_dir\e[0m directory..."
-    mkdir -p "$ssh_dir"
-  fi
-
-  if [ -f "$authorized_keys" ]; then
-    display_success "The \e[1m$authorized_keys\e[0m file already exists."
-  else
-    display_info "Creating the \e[1m$authorized_keys\e[0m file..."
-    touch "$authorized_keys"
-  fi
-
-  if grep -q "$public_key" "$authorized_keys"; then
-    display_success "The public key already exists in \e[1mauthorized_keys\e[0m."
-  else
-    echo "$public_key" >> "$authorized_keys"
-    display_success "SSH public key added to \e[1mauthorized_keys\e[0m."
-  fi
+  [[ -d "$ssh_dir" ]] || mkdir -p "$ssh_dir"
+  [[ -f "$authorized_keys" ]] || touch "$authorized_keys"
+  grep -q "$public_key" "$authorized_keys" || echo "$public_key" >> "$authorized_keys"
 }
 
-# Function to create a new user
 create_user() {
-  local username=$1
-  local password=$2
+  username=$1
+  password=$2
 
-  if id -u "$username" >/dev/null 2>&1; then
-    display_error "User '$username' already exists. Skipping user creation."
-    return
-  fi
-
-  display_info "Creating user '$username'..."
+  id -u "$username" >/dev/null 2>&1 && return
   sudo useradd -m -s /bin/bash "$username"
-  display_success "User '$username' created."
-
-  if [[ -n "$password" ]]; then
-    display_info "Setting password for user '$username'..."
-    echo "$username:$password" | sudo chpasswd
-    display_success "Password set for user '$username'."
-  fi
-
-  display_info "Granting sudo privileges to user '$username'..."
+  [[ -n "$password" ]] && echo "$username:$password" | sudo chpasswd
   sudo usermod -aG sudo "$username"
-  display_success "Sudo privileges granted to user '$username'."
-
-  if command -v docker >/dev/null 2>&1; then
-    display_info "Granting docker privileges to user '$username'..."
-    sudo usermod -aG docker "$username"
-    display_success "Docker privileges granted to user '$username'."
-  fi
+  command -v docker >/dev/null 2>&1 && sudo usermod -aG docker "$username"
 }
 
-# Function to configure SSH server
 configure_ssh() {
-  display_info "Updating SSH server configuration: disabling root login, enabling public key authentication, and disabling password authentication."
   sudo sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' \
          -e 's/#PubkeyAuthentication/PubkeyAuthentication/g' \
          -e 's/#AuthorizedKeysFile/AuthorizedKeysFile/g' \
          -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' \
          /etc/ssh/sshd_config
-  display_success "SSH server configuration updated."
-
-  display_info "Restarting SSH service..."
   sudo systemctl restart ssh
-  display_success "SSH service restarted."
 }
 
-# Function to configure system settings
 configure_system() {
-  display_info "Updating system configuration: setting hostname to 'EuangeLion', updating /etc/hosts file, generating locale settings, and setting timezone to Asia/Hong_Kong."
   sudo hostnamectl set-hostname EuangeLion
   sudo sed -i 's/localhost/EuangeLion localhost/g' /etc/hosts
   sudo sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
-  sudo locale-gen  > /dev/null 2>&1
+  sudo locale-gen
   sudo timedatectl set-timezone Asia/Hong_Kong
-  display_success "System configuration updated."
 }
 
-# Function to setup the environment
 setup_environment() {
-  display_info "Setting up the environment..."
-
   install_package zsh curl
-
-  display_info "Installing pip..."
-  sudo curl -sSL https://bootstrap.pypa.io/get-pip.py | sudo python3 > /dev/null 2>&1
-  display_success "Pip installed."
-
-  display_info "Installing wakatime..."
-  sudo pip3 install wakatime > /dev/null 2>&1
-  display_success "Wakatime installed."
-
-  display_info "Configuring Zsh..."
-  curl -o ~/.zshrc https://s.repo.host/addons/zshrc > /dev/null 2>&1
-  display_success "Zsh configured."
-
-  display_info "Changing default shell..."
+  sudo curl -sSL https://bootstrap.pypa.io/get-pip.py | sudo python3
+  sudo pip3 install wakatime
+  curl -o ~/.zshrc https://s.repo.host/addons/zshrc
   sudo chsh -s $(command -v zsh) "$current_user"
-  display_success "Default shell changed."
-
-  display_success "Environment setup completed successfully."
-
   zsh
 }
 
-# Function to install BBR
 install_bbr() {
-  local setup=$1
-
-  if [[ -f "/etc/debian_version" ]]; then
-    if [[ "$setup" == "-s" ]]; then
-      display_success "Enabling BBR..."
-      echo "net.core.default_qdisc = fq" | sudo tee -a /etc/sysctl.conf > /dev/null
-      echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.conf > /dev/null
-      sudo sysctl -p > /dev/null
-      display_success "BBR enabled."
-    else
-      display_info "Downloading BBR kernel..."
-      curl -o /tmp/linux-headers-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb https://s.repo.host/addons/linux-headers-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb
-      curl -o /tmp/linux-image-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb https://s.repo.host/addons/linux-image-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb
-      display_success "BBR kernel downloaded."
-
-      display_info "Installing BBR kernel..."
-      sudo apt install /tmp/linux-*-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb > /dev/null 2>&1
-      display_success "BBR kernel installed."
-
-      rm /tmp/linux-*-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb
-    fi
+  setup=$1
+  [[ -f "/etc/debian_version" ]] || return
+  if [[ "$setup" == "-s" ]]; then
+    echo "net.core.default_qdisc = fq" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -p
   else
-    display_error "Unsupported distribution. Unable to install BBR."
-    return 1
+    curl -o /tmp/linux-headers.deb https://s.repo.host/addons/linux-headers.deb
+    curl -o /tmp/linux-image.deb https://s.repo.host/addons/linux-image.deb
+    sudo apt install /tmp/linux-*.deb
+    rm /tmp/linux-*.deb
   fi
 }
 
-# Function to install Docker
 install_docker() {
-  display_info "Installing Docker: checking if Docker is already installed, installing Docker if necessary, and granting Docker privileges to user '$current_user'."
-  if command -v docker >/dev/null 2>&1; then
-    display_info "Docker is already installed, skipping installation."
-  else
-    curl -sSL https://get.docker.com | sh > /dev/null 2>&1
-    display_success "Docker installed."
-  fi
-
-  if [[ $current_user != "root" ]]; then
-    display_info "Granting Docker privileges to user '$current_user'..."
-    sudo usermod -aG docker "$current_user"
-    display_success "Docker privileges granted to user '$current_user'."
-  fi
+  command -v docker >/dev/null 2>&1 && return
+  curl -sSL https://get.docker.com | sh
+  [[ $current_user != "root" ]] && sudo usermod -aG docker "$current_user"
 }
 
-# Function to reinstall Debian
 reinstall_debian() {
-  display_info "Installing Debian: using the specified mirror for installation."
-  local mirror="http://deb.debian.org/debian"
-  if [[ $(curl -s api.baka.cafe?isCN) == '1' ]]; then
-    display_success "Region: \e[1mChina\e[0m, setting the system repo to \e[1mUSTC\e[0m."
-    mirror="http://mirrors.ustc.edu.cn/debian"
-  fi
+  mirror="http://deb.debian.org/debian"
+  [[ $(curl -s api.baka.cafe?isCN) == '1' ]] && mirror="http://mirrors.ustc.edu.cn/debian"
   curl -sSL https://s.repo.host/addons/InstallNET.sh | sudo bash -s -- -d 12 -v 64 -a --mirror "$mirror" -p 'repo.host'
-  display_success "Rebooting..."
 }
 
-# Function to install Caddy
 install_caddy() {
-  display_info "Installing Caddy..."
-
   install_package gnupg apt-transport-https lsb-release
-
   curl -sSL https://dl.cloudsmith.io/public/caddy/stable/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/caddy.gpg >/dev/null
-
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/caddy.gpg] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/caddy.list >/dev/null
-
   install_package caddy
-
-  display_success "Caddy installed successfully."
-
-  display_info "Please configure Caddy by editing the Caddyfile located at /etc/caddy/Caddyfile."
-  display_info "For more information on configuring Caddy, refer to the official Caddy documentation."
 }
 
-
-declare -g current_user=$(whoami)
-
+current_user=$(whoami)
 check_root_privileges
+[[ -z "$1" ]] && print_help
 
-if [ -z "$1" ]; then
-  print_help
-fi
-
-# Check the argument and execute the corresponding function
 case "$1" in
   ssh-key)         configure_ssh_key ;;
   ssh)             configure_ssh ;;
@@ -251,5 +138,5 @@ case "$1" in
   bbr)             shift; install_bbr "$@" ;;
   caddy)           install_caddy ;;
   create-user)     shift; create_user "$@" ;;
-  help|*)          print_help ;;
+  *)               print_help ;;
 esac
