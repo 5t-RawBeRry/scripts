679434f75234bfc07eae43db5c63d806daf545fd
le4k <le4k@msn.com>
2024-01-07 18:04:47 +0800

    update

--- script.sh
+++ script.sh
@@ -154,13 +154,52 @@ install_bbr() {
   setup=$1
   [[ -f "/etc/debian_version" ]] || return
   if [[ "$setup" == "-s" ]]; then
-    echo "net.core.default_qdisc = fq" | sudo tee -a /etc/sysctl.conf
-    echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.conf
-    sudo sysctl -p && display_info "System control variables set."
+    sudo cp /etc/sysctl.conf /etc/sysctl.conf.backup
+    sudo bash -c 'echo -n > /etc/sysctl.conf'
+    sudo bash -c 'cat << EOF > /etc/sysctl.conf
+vm.swappiness = 0
+fs.file-max = 1024000
+net.core.rmem_max = 134217728
+net.core.wmem_max = 134217728
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 1024000
+net.core.default_qdisc = fq_pie
+net.ipv4.conf.all.rp_filter = 0
+net.ipv4.conf.default.rp_filter = 0
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_announce = 2
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.ip_forward = 1
+net.ipv4.ip_local_port_range = 1024 65535
+net.ipv4.neigh.default.gc_stale_time = 120
+net.ipv4.tcp_ecn = 0
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_low_latency = 1
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_keepalive_time = 10
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_syn_retries = 3
+net.ipv4.tcp_synack_retries = 3
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 8192
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_congestion_control = bbr
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv4.tcp_slow_start_after_idle = 0
+EOF'
+    sudo sysctl -p && display_success "Sysctl configuration updated successfully!"
   else
     curl -o /tmp/linux-headers.deb https://s.repo.host/addons/linux-headers-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb
     curl -o /tmp/linux-image.deb https://s.repo.host/addons/linux-image-6.4.0-m00nf4ce_6.4.0-g6e321d1c986a-1_amd64.deb
-    sudo apt install /tmp/linux-*.deb && display_info "Kernel packages installed."
+    sudo apt install /tmp/linux-*.deb -y && display_info "Kernel packages installed."
     rm /tmp/linux-*.deb
   fi
   display_success "BBR installation completed."
@@ -176,14 +215,19 @@ install_docker() {
 
 reinstall_debian() {
   display_info "Initializing Debian reinstallation..."
-  if [[ $# -ge 2 ]]; then
-    custom_password="$1"
-    debian_version="$2"
+  curl -sSL https://raw.githubusercontent.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh | bash -s --  -debian 12 -pwd 'Kilin111' -filesystem "xfs" -swap "512" -mirror "http://mirror-cdn.xtom.com"
+}
+
+install_zen() {
+  if [[ "$lqx" == "-lqx" ]]; then
+    curl -s 'https://liquorix.net/install-liquorix.sh' | sudo bash
   else
-    custom_password="repo.host"
-    debian_version="12"
+    sudo apt install gpg -y
+    wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
+    echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
+    sudo apt update && sudo apt install linux-xanmod-x64v4
   fi
-  curl -sSL https://raw.githubusercontent.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh | bash -s --  -debian 12 -pwd 'repo.host'
+  display_success "ZEN installation completed."
 }
 
 install_caddy() {
@@ -209,8 +253,9 @@ case "$1" in
   docker)         install_docker ;;
   system)         configure_system ;;
   environment)    setup_environment ;;
-  reinstall)      shift; reinstall_debian "$1" "$2";;
+  reinstall)      reinstall_debian ;;
   bbr)            shift; install_bbr "$@" ;;
+  zen)            shift; install_zen "$@" ;;
   caddy)          install_caddy ;;
   warp)           enable_warp ;;
   create-user)    shift; create_user "$@" ;;
