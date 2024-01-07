dc2c6f56f345aa783fc6d717d6a21f930b1e9d2c
le4k <le4k@msn.com>
2024-01-08 02:42:55 +0800

    update

--- script.sh
+++ script.sh
@@ -222,7 +222,7 @@ install_zen() {
   if [[ "$1" == "-lqx" ]]; then
     curl -s 'https://liquorix.net/install-liquorix.sh' | sudo bash
   else
-    sudo apt install gpg -y
+    sudo apt install gpg wget -y
     wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
     echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
     sudo apt update && sudo apt install linux-xanmod-x64v4
