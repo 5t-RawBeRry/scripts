92684b66c0c7438177781130d15180a4cf650916
StrawBerry <hs_err@msn.com>
2024-06-10 02:40:26 +0800

    Update debian-init.sh

--- addons/debian-init.sh
+++ addons/debian-init.sh
@@ -109,14 +109,43 @@ sed -i "s/^::1.*$/::1             $new_hostname localhost localhost.localdomain 
 # Enable BBR and additional network optimizations
 display_message $highlight_color "\nEnabling BBR and network optimizations..."
 cat <<EOF >> /etc/sysctl.conf
-net.core.default_qdisc=fq_pie
-net.ipv4.tcp_congestion_control=bbr
-net.ipv4.tcp_fastopen=3
-net.ipv4.tcp_tw_reuse=1
-net.ipv4.tcp_fin_timeout=30
-net.core.netdev_max_backlog=250000
-net.core.somaxconn=65535
-net.ipv4.tcp_max_syn_backlog=8192
+vm.swappiness = 0
+fs.file-max = 1024000
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 1024000
+net.core.default_qdisc = fq_pie
+net.ipv4.conf.all.rp_filter = 0
+net.ipv4.conf.default.rp_filter = 0
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_announce = 2
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.ip_forward = 1
+net.ipv4.ip_local_port_range = 1024 65535
+net.ipv4.neigh.default.gc_stale_time = 120
+net.ipv4.tcp_ecn = 0
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_low_latency = 1
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_keepalive_time = 10
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_syn_retries = 3
+net.ipv4.tcp_synack_retries = 3
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 8192
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_congestion_control = bbr
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv4.tcp_slow_start_after_idle = 0
 EOF
 sysctl -p
 
